73064520921c408e5bc886f185af941fd1aaa4e9
thechilguy <thechichillguy@gmail.com>
2026-02-26 13:35:26 +0200

    v0.2

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get , Param, Post} from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('hello/:name')
+  getHello(@Param('name') name:string): string {
+    return this.appService.getHello(name);
+  }
+
+  @Post()
+  createHello(): string {
+    return "Hello"
   }
 }
